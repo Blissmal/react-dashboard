2d0171dc88c7421d3aee0eabd5b32b3e19d6a0d8
Blissmal <malutibethuel@gmail.com>
2024-05-21 15:00:43 +0300

    added components

--- src/components/TeacherList.jsx
+++ src/components/TeacherList.jsx
@@ -12,17 +12,30 @@ const teachers = [
 ]
 
 const TeacherList = () => {
-  return (
-    <div className='teacher--list'>
-      <div className="list--header">
-        <h2>Teachers</h2>
-        <select name="" id="">
-            <option value="english">English</option>
-            <option value="spanish">Spanish</option>
-        </select>
-      </div>
-    </div>
-  )
+    return (
+        <div className='teacher--list'>
+            <div className="list--header">
+                <h2>Teachers</h2>
+                <select name="" id="">
+                    <option value="english">English</option>
+                    <option value="spanish">Spanish</option>
+                </select>
+            </div>
+            <div className="list--container">
+                {teachers.map((teacher, index) => (
+                    <div className="list">
+                        <div className="teacher--details">
+                            <img src={teacher.image} alt={teacher.name} />
+                            <h2>{teacher.name}</h2>
+                        </div>
+                        <span>{teacher.duration}</span>
+                        <span>{teacher.cost}</span>
+                        <span className='teacher--todo'>:</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default TeacherList
