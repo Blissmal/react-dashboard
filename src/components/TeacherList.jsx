512bee863b28d983f43958ee41c1cc510150b9f9
Blissmal <malutibethuel@gmail.com>
2024-05-21 14:51:43 +0300

    updated

--- src/components/TeacherList.jsx
+++ src/components/TeacherList.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "../styles/teacherList.css"
+import Image1 from '../assets/download.jpg'
+
+const teachers = [
+    {
+        image: Image1,
+        name: 'Prof. John Doe',
+        duration: '20 hours lesson',
+        cost: '100'
+    }
+]
 
 const TeacherList = () => {
   return (
-    <div>
-      
+    <div className='teacher--list'>
+      <div className="list--header">
+        <h2>Teachers</h2>
+        <select name="" id="">
+            <option value="english">English</option>
+            <option value="spanish">Spanish</option>
+        </select>
+      </div>
     </div>
   )
 }
